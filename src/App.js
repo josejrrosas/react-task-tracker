3dffcae1931c2c40c417a243e337d9e6d457d5c1
Jose Rosas <josejrrosas@yahoo.com>
2021-11-15 22:42:32 -0600

    created task and tasks components, displaying with useState  and map method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import Header from './components/Header'
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-  return (
-   <div>
-     <Header />
-   </div>
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at job",
+      day: "Mar 10th at 4:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Meeting at work",
+      day: "Dec 8th at 7:30pm",
+      reminder: true,
+    },
+  ]);
 
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log("delete", id);
+  };
+
+  return (
+    <div>
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
+    </div>
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Button = () => {
-    return (
-        <div>
-            <button className ='btn'>Add</button>
-        </div>
-    )
-}
+const Button = ({ color, text, onClick }) => {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
 
-export default Button
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
